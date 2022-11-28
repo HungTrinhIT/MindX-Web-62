f1993e40c702adec2f7a7ac52b438857dcbdcdb1
HungTrinhJH <hung.trinh@journeyh.io>
2022-11-28 21:14:38 +0700

    add middlewares

--- lesson-5-expressjs/server.js
+++ lesson-5-expressjs/server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const contactRouter = require("./routes/contacts");
 const userRouter = require("./routes/users");
+
+const checkAdminMiddleware = require("./middlewares/checkAdmin");
+
 const app = express();
 const PORT = 3001;
 
@@ -9,6 +12,45 @@ app.use(express.json());
 
 app.use("/contacts", contactRouter);
 app.use("/users", userRouter);
+
+app.get("/admin", checkAdminMiddleware, (req, res) => {
+  res.send("Welcome to admin page");
+});
+
+const middleWareVongSoLoai = (req, res, next) => {
+  if (true) {
+    console.log("Qua được vòng sơ loại rồi,.... Go Go");
+    next();
+  }
+};
+
+const middleWareVongBang = (req, res, next) => {
+  if (true) {
+    console.log("Qua được vòng bảng rồi,.... Go Go");
+    next();
+    // res.json({
+    //   msg: "Bị loại ở vòng bảng rồi",
+    // });
+  }
+};
+const middleWareVong1_16 = (req, res, next) => {
+  if (true) {
+    console.log("Qua được vòng 1/16 rồi,.... Go Go");
+    next();
+  }
+};
+
+app.get(
+  "/lay-cup-ve-brasil",
+  middleWareVongSoLoai,
+  middleWareVongBang,
+  middleWareVong1_16,
+  (req, res) => {
+    res.json({
+      msg: "Winnnn, mang cúp về nước đê",
+    });
+  }
+);
 
 app.listen(PORT, () => {
   console.log(`Server is listening at PORT ${PORT}`);
@@ -50,4 +92,15 @@ app.listen(PORT, () => {
       /worldcup => 404 Not Found
 
     7 * 5 = 35 APIs * 20  = 700 code lines
+
+    Brasil 2022 Quatar 
+      => Vòng sơ loại
+      => Vòng bảng
+      => Vòng 1/16
+      => Vòng tứ kết
+      => Vòng bán kết
+      => Trận chung kêt
+      => Cup
+
+    Brasil => /lay-cup-ve-nuoc 
 */
