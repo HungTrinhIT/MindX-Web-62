c41826e711e68d7416c925c927d8ef103bc4fbde
HungTrinhJH <hung.trinh@journeyh.io>
2022-12-09 19:06:43 +0700

    feat: update private route

--- contact-keeper-mern-stack/client/src/components/PrivateRoute/PrivateRoute.js
+++ contact-keeper-mern-stack/client/src/components/PrivateRoute/PrivateRoute.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Navigate } from "react-router-dom";
+import AuthContext from "../../contexts/AuthState/AuthContext";
 
-const PrivateRoute = () => {
-  return <div>PrivateRoute</div>;
+const PrivateRoute = ({ component: Component }) => {
+  const { state } = useContext(AuthContext);
+  const { isAuthenticated } = state;
+
+  if (isAuthenticated) {
+    return <Component />;
+  }
+
+  return <Navigate to="/login" />;
 };
 
 export default PrivateRoute;
